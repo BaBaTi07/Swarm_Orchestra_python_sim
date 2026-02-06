31e686913443bb35aa714d89d5d63e9cc26ca682
BaBaTi07 <baptiste.moncomble@gmail.com>
2026-02-06 17:40:32 +0100

    improve read_json.py

--- json_files/read_json.py
+++ json_files/read_json.py
@@ -3,44 +3,55 @@ import numpy as np
 from world.world import Arena
 from exp.experiment_1 import Exp
 
+#TODO : handle json objects in any order
+#TODO : handle missing json objects (e.g. no round_obstacle, no cuboid_obstacle, etc.)
+#TODO : handle wrong json objects (e.g. wrong keys, wrong values, etc.)
+
 def read_json_file (file_name):
     try:
         with open(file_name, 'r') as file:
             data = json.load(file)
-            for k, v in data["experiment"][0]["duration"].items():
-                if k == "num_trials":
-                    Exp.num_trials = v
-                    # print(f"Trials = {Exp.num_trials}")
-                elif k == "num_iterations":
-                    Exp.num_iterations = v
-                    # print(f"Iterations = {Exp.num_iterations}")
- 
-            for k, v in data["experiment"][1]["arena"].items():
-                # print(k)
-                if k == "delta_t":
-                    Arena.DeltaT = v 
-                    # print(Arena.DeltaT)
-                elif k == "perimetral round wall [x,y,z,radius,height,colour]":
-                    Arena.perimetral_wall_xyz_radius_height_colour = np.array(v) 
-                    # print(Arena.perimetral_wall_xyz_radius_height_colour)
-
-            for k, v in data["experiment"][2]["round_obstacle"].items():
-                # print(k)
-                if k == "[x,y,z,radius,h,colour]":
-                    Arena.round_obst_xyz_radius_height_colour = np.array(v) 
-                    # print(Arena.round_obst_xyz_radius_h_colour)
-                    
-            for k, v in data["experiment"][3]["cuboid_obstacle"].items():
-                # print(k)
-                if k == "[x,y,z,l,w,h,rx,ry,rz,colour]":
-                    Arena.cuboid_obst_xyz_lwh_rxryrz_colour = np.array(v)
-                    # print(Arena.cuboid_obst_xyz_lwh)
-                
-            for k, v in data["experiment"][4]["e_pucks"].items():
-                # print(k)
-                if k == "[x,y,z,rot_x,rot_y,rot_z,colour]":
-                    Arena.epucks_xyz_rx_ry_rz_colour = np.array(v) 
-                    # print(Arena.epucks_xyz_rx_ry_rz_colour)
-                    
     except FileNotFoundError:
-        print("Error: The file 'world.json' was not found.")
+        raise FileNotFoundError(
+            f"Error: The file {file_name} was not found."
+            "\nSimulation cannot start without a valid JSON configuration file."
+        )
+    except json.JSONDecodeError as e:
+        raise ValueError(
+            f"Invalid JSON format in '{file_name}': {e}"
+        )
+    for k, v in data["experiment"][0]["duration"].items():
+        if k == "num_trials":
+            Exp.num_trials = v
+            # print(f"Trials = {Exp.num_trials}")
+        elif k == "num_iterations":
+            Exp.num_iterations = v
+            # print(f"Iterations = {Exp.num_iterations}")
+
+    for k, v in data["experiment"][1]["arena"].items():
+        # print(k)
+        if k == "delta_t":
+            Arena.DeltaT = v 
+            # print(Arena.DeltaT)
+        elif k == "perimetral round wall [x,y,z,radius,height,colour]":
+            Arena.perimetral_wall_xyz_radius_height_colour = np.array(v) 
+            # print(Arena.perimetral_wall_xyz_radius_height_colour)
+
+    for k, v in data["experiment"][2]["round_obstacle"].items():
+        # print(k)
+        if k == "[x,y,z,radius,h,colour]":
+            Arena.round_obst_xyz_radius_height_colour = np.array(v) 
+            # print(Arena.round_obst_xyz_radius_h_colour)
+            
+    for k, v in data["experiment"][3]["cuboid_obstacle"].items():
+        # print(k)
+        if k == "[x,y,z,l,w,h,rx,ry,rz,colour]":
+            Arena.cuboid_obst_xyz_lwh_rxryrz_colour = np.array(v)
+            # print(Arena.cuboid_obst_xyz_lwh)
+        
+    for k, v in data["experiment"][4]["e_pucks"].items():
+        # print(k)
+        if k == "[x,y,z,rot_x,rot_y,rot_z,colour]":
+            Arena.epucks_xyz_rx_ry_rz_colour = np.array(v) 
+            # print(Arena.epucks_xyz_rx_ry_rz_colour)
+            
